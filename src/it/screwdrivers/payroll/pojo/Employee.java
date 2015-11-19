ce7d4ee70a69104a67bee830c73a8b3d05ec5a19
Alessandro Gatti <alemicio91@gmail.com>
2015-11-19 17:43:25 +0100

    dio

--- src/it/screwdrivers/payroll/pojo/Employee.java
+++ src/it/screwdrivers/payroll/pojo/Employee.java
@@ -46,7 +46,7 @@ public abstract class Employee implements Serializable{
 	@JoinColumn(name="employeeId")
 	private Paymethod paymethod;
 	
-	// TO DO un po di più
+	// TO DO un po di più bene
 	@ManyToOne
 	private Union union;
 	
