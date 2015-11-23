b995172b79e59aa409ff59c13e6e72b7ac72f8df
ericvilla <eric.villa91@gmail.com>
2015-11-23 12:12:36 +0100

    Added UnionServiceAssociationDao

---
+++ src/it/screwdrivers/payroll/dao/UnionServiceAssociationDao.java
@@ -0,0 +1,33 @@
+package it.screwdrivers.payroll.dao;
+
+import it.screwdrivers.payroll.pojo.union.UnionServiceAssociation;
+
+import java.util.List;
+
+import javax.ejb.Stateless;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+@Stateless
+public class UnionServiceAssociationDao {
+
+	@PersistenceContext
+	EntityManager em;
+
+	public void add(UnionServiceAssociation union_service_association) {
+		em.persist(union_service_association);
+	}
+
+	public List<UnionServiceAssociation> findAll() {
+		List<UnionServiceAssociation> union_service_associations = em.createQuery("select p from UnionServiceAssociation s", UnionServiceAssociation.class).getResultList();
+		return union_service_associations;
+	}
+
+	public UnionServiceAssociation update(UnionServiceAssociation union_service_association) {
+		return em.merge(union_service_association);
+	}
+
+	public void remove(UnionServiceAssociation union_service_association) {
+		em.remove(union_service_association);
+	}
+}
