481fba2702825e7cf521348ee317b2d156b543d6
ericvilla <eric.villa91@gmail.com>
2015-11-23 11:55:38 +0100

    Added PaymethodDao
    Added PaymethodDao with add, findAll, update, remove methods.

---
+++ src/it/screwdrivers/payroll/dao/PaymethodDao.java
@@ -0,0 +1,33 @@
+package it.screwdrivers.payroll.dao;
+
+import it.screwdrivers.payroll.pojo.payment.Paymethod;
+
+import java.util.List;
+
+import javax.ejb.Stateless;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+@Stateless
+public class PaymethodDao {
+
+	@PersistenceContext
+	EntityManager em;
+
+	public void add(Paymethod paymethod) {
+		em.persist(paymethod);
+	}
+
+	public List<Paymethod> findAll() {
+		List<Paymethod> paymethods = em.createQuery("select p from Paymethod s", Paymethod.class).getResultList();
+		return paymethods;
+	}
+
+	public Paymethod update(Paymethod paymethod) {
+		return em.merge(paymethod);
+	}
+
+	public void remove(Paymethod paymethod) {
+		em.remove(paymethod);
+	}
+}
