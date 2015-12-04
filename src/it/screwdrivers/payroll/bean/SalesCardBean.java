426a37b4ce13c052462f32fa6e977aa38aa32b16
simone91gv <simone91gv@gmail.com>
2015-12-04 09:26:27 +0100

    Add return statement that check if the employee tries to send more than one time a salescard referers to a customer on a specific date.

--- src/it/screwdrivers/payroll/bean/SalesCardBean.java
+++ src/it/screwdrivers/payroll/bean/SalesCardBean.java
@@ -16,49 +16,58 @@ import javax.inject.Named;
 
 @Named("salescard")
 @SessionScoped
-public class SalesCardBean implements Serializable{
+public class SalesCardBean implements Serializable {
 
 	private static final long serialVersionUID = 1L;
 
 	@Inject
 	SalesCardController s_controller;
-	
+
 	@Inject
 	SalesCard sales_card;
 	private String response;
-	
+
 	private Date date;
 	private float amount;
 	private String customer;
-	
-	
-	
-	public void submitSalesCard(CommissionedEmployee commissioned_employee){
-		
+
+	public void submitSalesCard(CommissionedEmployee commissioned_employee) {
+
 		sales_card.setDate(date);
 		sales_card.setAmount(amount);
 		sales_card.setCustomer(customer);
 		sales_card.setCommissioned_employee(commissioned_employee);
-		
-		response = s_controller.registerTimeCard(commissioned_employee,sales_card);
-		
+
+		response = s_controller.registerTimeCard(commissioned_employee,
+				sales_card);
+
 		if (response == "success") {
 			FacesContext context = FacesContext.getCurrentInstance();
 			context.addMessage(null, new FacesMessage(
 					FacesMessage.SEVERITY_INFO, "Congratulations",
 					"your SalesCard is correctly sent"));
 		}
+		if (response == "failed") {
+			FacesContext context = FacesContext.getCurrentInstance();
+			context.addMessage(null, new FacesMessage(
+					FacesMessage.SEVERITY_ERROR, "ERROR",
+					"A sales card from that customer was already"
+					+"sent for this date"));
+		}
 	}
-	
+
 	public Date getDate() {
 		return date;
 	}
+
 	public void setDate(Date date) {
 		this.date = date;
 	}
+
 	public float getAmount() {
 		return amount;
 	}
+
 	public void setAmount(float amount) {
 		this.amount = amount;
 	}
@@ -70,8 +79,5 @@ public class SalesCardBean implements Serializable{
 	public void setCustomer(String customer) {
 		this.customer = customer;
 	}
-	
-	
-	
 
 }
