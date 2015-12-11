1eaff4d061bf6d5969b25dc7ea1db0a00641ee6d
simone91gv <simone91gv@gmail.com>
2015-12-11 13:07:21 +0100

    add the client PayrollEngine

--- src/it/screwdrivers/payroll/engine/PayEngineFactory.java
+++ src/it/screwdrivers/payroll/engine/PayEngineFactory.java
@@ -1,22 +1,28 @@
 package it.screwdrivers.payroll.engine;
 
+import it.screwdrivers.payroll.pojo.employee.CommissionedEmployee;
+import it.screwdrivers.payroll.pojo.employee.ContractorEmployee;
+import it.screwdrivers.payroll.pojo.employee.Employee;
+import it.screwdrivers.payroll.pojo.employee.EmployeeManager;
+import it.screwdrivers.payroll.pojo.employee.SalariedEmployee;
+
 public class PayEngineFactory {
 
-	public IPayEngine getPayEngine(String engineType) {
+	public IPayEngine getPayEngine(Employee employee) {
 
-		if (engineType == null) {
+		if (employee == null) {
 			return null;
 		}
-		if (engineType == "Commissioned") {
+		if (employee instanceof CommissionedEmployee) {
 			return new CommissionedPayEngine();
 
-		} else if (engineType == "Contractor") {
+		} else if (employee instanceof ContractorEmployee) {
 			return new ContractorPayEngine();
 
-		} else if (engineType == "Salaried") {
+		} else if (employee instanceof SalariedEmployee) {
 			return new SalariedPayEngine();
 			
-		} else if (engineType == "Manager") {
+		} else if (employee instanceof EmployeeManager) {
 			return new ManagerPayEngine();
 		}
 		
---
+++ src/it/screwdrivers/payroll/engine/PayrollEngine.java
@@ -0,0 +1,12 @@
+package it.screwdrivers.payroll.engine;
+
+public class PayrollEngine {
+	
+	PayEngineFactory pay_engine_factory = new PayEngineFactory();
+	IPayEngine pay_engine;
+	
+	public void runPayroll(){
+		// TODO this method will run the payroll on the current date
+	}
+
+}
