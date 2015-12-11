1df34bb37343e1880f3076901c43bd4b79f6201c
simone91gv <simone91gv@gmail.com>
2015-12-11 15:39:13 +0100

    add skeletron

--- src/it/screwdrivers/payroll/engine/PayrollEngine.java
+++ src/it/screwdrivers/payroll/engine/PayrollEngine.java
@@ -1,19 +1,32 @@
 package it.screwdrivers.payroll.engine;
 
-import java.util.Calendar;
-import java.util.Date;
-import java.util.GregorianCalendar;
-
 public class PayrollEngine {
-	
+
 	PayEngineFactory pay_engine_factory = new PayEngineFactory();
 	IPayEngine pay_engine;
 	PayrollCalendar c = new PayrollCalendar();
-	
-	public void runPayroll(){
-		
-		if(c.isFriday()){
+
+	public void runPayroll() {
+
+		if (c.isFriday()) {
+
+			// TODO pay contractor employees
+
+			if (c.isWeekNumberPair()) {
+				// TODO pay commisioned employees commissions
+			}
+
+			if (c.d3ChangeMounth()) {
+				// TODO pay salaried employees
+				// TODO Pat commissioned standard salary
+			}
+		}
+		else{
 			
+			if(c.d1ChangeMounth()){
+				// TODO pay salaried employees
+				// TODO Pat commissioned standard salary
+			}
 		}
 	}
 
