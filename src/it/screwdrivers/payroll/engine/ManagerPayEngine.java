dfc761bb39ad0062a873e475477c3202421f1dcc
simone91gv <simone91gv@gmail.com>
2015-12-14 15:55:36 +0100

    also for manager type

--- src/it/screwdrivers/payroll/engine/ManagerPayEngine.java
+++ src/it/screwdrivers/payroll/engine/ManagerPayEngine.java
@@ -3,6 +3,7 @@ package it.screwdrivers.payroll.engine;
 import java.util.List;
 
 import it.screwdrivers.payroll.controller.HistoricalSalarycontroller;
+import it.screwdrivers.payroll.controller.HistoricalUnionChargeController;
 import it.screwdrivers.payroll.dao.EmployeeDao;
 import it.screwdrivers.payroll.pojo.employee.EmployeeManager;
 
@@ -13,16 +14,28 @@ public class ManagerPayEngine implements IPayEngine {
 	@Inject
 	EmployeeDao e_dao;
 	@Inject
+	HistoricalUnionChargeController huc_controller;
+	@Inject
 	HistoricalSalarycontroller h_controller;
 
 	@Override
 	public void pay() {
 
 		List<EmployeeManager> m_employees = e_dao.findAllManager();
+		float dues = 0;
+		float total_charges = 0;
 
 		for (EmployeeManager m : m_employees) {
 
-			h_controller.registerPay(m);
+			if(m.getUnion() == null){
+				h_controller.registerPay(m);
+			}
+			else {
+				dues = m.getAnnual_rate()* m.getUnion().getUnion_dues();
+				total_charges = huc_controller.UnionChargeByEmployee(m);
+				h_controller.registerPay(m, (total_charges+dues) );
+				
+			}
 
 		}
 
