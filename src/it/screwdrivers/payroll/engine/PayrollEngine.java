48579cf687878e089604af8987a160b15fd07237
simone91gv <simone91gv@gmail.com>
2015-12-15 10:56:13 +0100

    add new class that test if a task is running pariodically when the server is runnig. But the problem about the nullpointer exception still remain. It causes the error in the payengine of every employee because returns null the injected instance of the e_dao.

--- src/it/screwdrivers/payroll/engine/PayrollEngine.java
+++ src/it/screwdrivers/payroll/engine/PayrollEngine.java
@@ -1,32 +1,44 @@
 package it.screwdrivers.payroll.engine;
 
+import javax.ejb.Schedule;
+import javax.ejb.Singleton;
+
 import it.screwdrivers.payroll.pojo.employee.CommissionedEmployee;
 import it.screwdrivers.payroll.pojo.employee.ContractorEmployee;
 import it.screwdrivers.payroll.pojo.employee.EmployeeManager;
 import it.screwdrivers.payroll.pojo.employee.SalariedEmployee;
 
+@Singleton
 public class PayrollEngine {
 
 	PayEngineFactory pay_engine_factory = new PayEngineFactory();
 	IPayEngine pay_engine;
 	PayrollCalendar c = new PayrollCalendar();
 
+	@Schedule(second="*/6", minute="*",hour="*", persistent=false)
 	public void runPayroll() {
+		
+		System.out.println("Payroll task running now");
+		System.out.println("E' venerdi?"+c.isFriday());
 
-		if (c.isFriday()) {
-
+		if (c.isFriday() == false) {
+			
+			System.out.println("is friday");
 			//  pay contractor employees
 			pay_engine = pay_engine_factory.getPayEngine(ContractorEmployee.class.getSimpleName());
 			pay_engine.pay();
 
 			if (c.isWeekNumberPair()) {
 				
+				System.out.println("is a pair week");
 				//pay commissions sales
 				pay_engine = pay_engine_factory.getPayEngine(CommissionedEmployee.class.getSimpleName());
 				pay_engine.pay();
 			}
 
 			if (c.d3ChangeMounth()) {
+				
+				System.out.println("d+3 change mounth");
 				//  pay salaried employees && commissioned standard salary
 				pay_engine = pay_engine_factory.getPayEngine(SalariedEmployee.class.getSimpleName());
 				pay_engine.pay();
@@ -36,6 +48,8 @@ public class PayrollEngine {
 		else{
 			
 			if(c.d1ChangeMounth()){
+				
+				System.out.println("d+1 change mounth");
 				//pay salaried employees && commissioned standard salary
 				pay_engine = pay_engine_factory.getPayEngine(SalariedEmployee.class.getSimpleName());
 				pay_engine.pay();
@@ -43,6 +57,8 @@ public class PayrollEngine {
 		}
 		
 		if(c.isLastDayOfYear()){
+			
+			System.out.println("is the last day of the year");
 			// TODO pay manager
 			pay_engine = pay_engine_factory.getPayEngine(EmployeeManager.class.getSimpleName());
 			pay_engine.pay();
