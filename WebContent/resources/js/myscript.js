76fa9dfaca9eeffd2498c18ff3c2d78d092244f5
ericvilla <eric.villa91@gmail.com>
2015-12-28 18:09:52 +0100

    added new change details form and its validation

--- WebContent/resources/js/myscript.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function() {
-
-	if ('#{union.getUnion(employee.retrived_employee)}' == 'Not setted') {
-		//alert("dioporco non è settato");
-
-	} else {
-		//alert("dioporco ha gia una union cazzo");
-	}
-});
