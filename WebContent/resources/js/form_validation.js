c789b8790cc6e957274b0074a6f6232c4273263a
ericvilla <eric.villa91@gmail.com>
2015-12-29 21:36:11 +0100

    Added empty field check for validation.

--- WebContent/resources/js/form_validation.js
+++ WebContent/resources/js/form_validation.js
@@ -1,6 +1,41 @@
 function validateForm() {
 	
-	alert("ciao");
+	var username = document.getElementById("change-detail-form:username").value;
+	var password = document.getElementById("change-detail-form:password").value;
+	var name = document.getElementById("change-detail-form:name").value;
+	var surname = document.getElementById("change-detail-form:surname").value;
+	var email = document.getElementById("change-detail-form:email").value;
+	var phoneNumber = document.getElementById("change-detail-form:phonenumber").value;
+	var postalAddress = document.getElementById("change-detail-form:postaladdress").value;
+	
+	if(username.length === 0) {
+		alert("Username field must be not empty.");
+		return false;
+		
+	} else if (password.length === 0) {
+		alert("Password field must be not empty.");
+		return false;
+		
+	} else if (name.length === 0) {
+		alert("Name field must be not empty.");
+		return false;
+		
+	} else if (surname.length === 0) {
+		alert("Surname field must be not empty.");
+		return false;
+		
+	} else if (email.length === 0) {
+		alert("Email field must be not empty.");
+		return false;
+		
+	} else if (phoneNumber.length === 0) {
+		alert("Phone Number field must be not empty.");
+		return false;
+		
+	} else if (postalAddress.length === 0) {
+		alert("Postal Address field must be not empty.");
+		return false;
+	}
 	
 	return true;
 }
